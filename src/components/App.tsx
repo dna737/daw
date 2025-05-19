091247a735a4fe235d4eeed695c173f1a46c7f09
dna737 <adnanhere27@gmail.com>
2025-05-19 09:19:03 -0500

    use single Navigate for root route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,22 +4,27 @@ import { Routes, Route, Navigate } from 'react-router'
 import Home from './core/Home'
 import { useAuth } from './hooks'
 import { AppContextProvider } from '../context/AppContext'
+import { Login } from './core'
 
 function App() {
-  // const { isLoggedIn } = useAuth();
-  const isLoggedIn = true;
+  const { isLoggedIn } = useAuth();
+  // const isLoggedIn = true;
 
   return (
     <>
       <AppContextProvider>
         <Routes>
-          <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" replace />} />
+          <Route path="/" element={
+            <Navigate to={isLoggedIn ? "/home" : "/login"} replace />
+          } />
           {/* TODO: Use DelayedRedirect for routes later */}
           {isLoggedIn ? (
             <>
+              <Route path="/home" element={<Home />} />
             </>
           ) : (
             <>
+              <Route path="/login" element={<Login />} />
             </>
           )}
         </Routes>
