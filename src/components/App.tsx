ed269df3ad0caad0004639ed5d1c8fe55147724c
dna737 <adnanhere27@gmail.com>
2025-05-19 10:20:52 -0500

    clean up routes and fix useAuth placement

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,35 +1,27 @@
 import { useState } from 'react'
 import "../App.css"
 import { Routes, Route, Navigate } from 'react-router'
-import Home from './core/Home'
 import { useAuth } from './hooks'
 import { AppContextProvider } from '../context/AppContext'
-import { Login } from './core'
+import { Login, Home } from './core'
 
-function App() {
+function AppRoutes() {
   const { isLoggedIn } = useAuth();
-  // const isLoggedIn = true;
 
   return (
-    <>
-      <AppContextProvider>
-        <Routes>
-          <Route path="/" element={
-            <Navigate to={isLoggedIn ? "/home" : "/login"} replace />
-          } />
-          {/* TODO: Use DelayedRedirect for routes later */}
-          {isLoggedIn ? (
-            <>
-              <Route path="/home" element={<Home />} />
-            </>
-          ) : (
-            <>
-              <Route path="/login" element={<Login />} />
-            </>
-          )}
-        </Routes>
-      </AppContextProvider>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to={isLoggedIn ? "/home" : "/login"} replace />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
+
+function App() {
+  return (
+    <AppContextProvider>
+      <AppRoutes />
+    </AppContextProvider>
   )
 }
 
