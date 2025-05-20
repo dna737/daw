a4cbb8401dab3ddb268e68a05fdff9bddb60585b
dna737 <adnanhere27@gmail.com>
2025-05-20 16:27:12 -0500

    get started with Dog Card

---
+++ src/components/core/DogCard.tsx
@@ -0,0 +1,22 @@
+import type { Dog } from "@/models";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card";
+
+export default function DogCard(props: { dog: Dog }) {
+  
+  const { dog } = props;
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+}
