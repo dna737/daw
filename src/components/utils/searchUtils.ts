5c7ec8616b8eef12f3d779ff81ba92593351b44b
dna737 <adnanhere27@gmail.com>
2025-05-23 16:36:39 -0500

    fix dog search results being unsorted

--- src/components/utils/searchUtils.ts
+++ src/components/utils/searchUtils.ts
@@ -20,10 +20,11 @@ export function getStateOptions(): StateOption[] {
 }
 
 export const filterBreedSearchItems = (breedSearchItems: DogSearchOption[], searchValue: string) => {
-  const filteredBreeds = breedSearchItems.filter((b) => b.name.toLowerCase().includes(searchValue.toLowerCase()));
-    const availableBreeds = filteredBreeds.filter((b) => !b.isSelected).sort((a, b) => a.name.localeCompare(b.name));
-    const selectedBreeds = filteredBreeds.filter((b) => b.isSelected).sort((a, b) => a.name.localeCompare(b.name));
-    return { availableBreeds, selectedBreeds };
+  const sortedItems = [...breedSearchItems].sort((a, b) => a.name.localeCompare(b.name));
+  const filteredBreeds = sortedItems.filter((b) => b.name.toLowerCase().includes(searchValue.toLowerCase()));
+  const availableBreeds = filteredBreeds.filter((b) => !b.isSelected);
+  const selectedBreeds = filteredBreeds.filter((b) => b.isSelected);
+  return { availableBreeds, selectedBreeds };
 };
 
 export const filterStateSearchItems = (stateOptions: StateOption[], searchValue: string) => {
